68d1ac9f938341b5f9c5e063313493709ca4eeff
Nick Balázs <99294505+Bahlazs@users.noreply.github.com>
2023-09-28 12:56:35 +0200

    add shell for frontend and styling

---
+++ secret-frontend/src/components/About.jsx
@@ -0,0 +1,14 @@
+import '../stylesheets/About.css'
+
+const About = () => {
+    return (
+        <div className={"about"}>
+            <h1>How this works?!</h1>
+            <p>Add your secret and retrieve them in a safe way.
+                After adding a secret you will get an encrypted code in e-mail.
+                Only with that code can you retrieve your secret!</p>
+        </div>
+    )
+}
+
+export default About
---
+++ secret-frontend/src/components/secret_components/SecretPage.jsx
@@ -0,0 +1,48 @@
+import "E:\\projects\\homework\\allmyles-ho\\Secret-server\\secret-frontend\\src\\stylesheets\\SecretPage.css"
+import {useState} from "react";
+
+const SecretPage = () => {
+    const [secret, setSecret] = useState("No Secret")
+    const [password, setPassword] = useState("")
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+    const getSecret = async () => {
+        setSecret("LOADING")
+        const response = await fetch("/secret/" + password, {
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json',
+            }
+        })
+        if (response.status === 200) {
+            setSecret(response.json())
+        } else {
+            alert("something went wrong")
+            setSecret("No Secret")
+        }
+    }
+
+    return (
+        <div className={"secret-page"}>
+            <h1>Get secret here</h1>
+            <div className={"password-field"}>
+                <input
+                    type="password"
+                    minLength={20}
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+                <button className={"submit-button"} type="button" onClick={getSecret}>Get Secret</button>
+            </div>
+            <div className={"secret-box"}>
+                <div className={"secret"}>{secret}</div>
+                <button className={"submit-button"}> Share Secret</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default SecretPage;
