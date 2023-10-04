8b3646fd4f69eda82ae770fa427ce384d5c1bb24
Nick Balázs <99294505+Bahlazs@users.noreply.github.com>
2023-10-04 15:51:14 +0200

    add corse

--- secret-api/app.py
+++ secret-api/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from blueprints.secret.secret_bp import secret_blueprint
 from db_operations.db_provider import get_db
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(secret_blueprint)
 app.config["SQLALCHEMY_DATABASE_URI"] = ("postgresql://postgres:secret-test-pw@db.ocscrttarmteeesaekdy.supabase.co"
                                          ":5432/postgres")
+CORS(app)
 db = get_db()
 db.init_app(app)
+
 
 if __name__ == '__main__':
     app.run(
